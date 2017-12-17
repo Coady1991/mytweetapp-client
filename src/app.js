4c7c888f905e45501b6673e7da6ec28757e67cf1
Niall Coady <niall_coady7@hotmail.com>
2017-12-17 20:02:45 +0000

    Simple login feature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+
+  email = 'homer@simpson.com';
+  password = 'secret';
+
+  loggedIn = false;
+
+  login() {
+    console.log(`Logging in ${this.email}`);
+    this.loggedIn = true;
   }
 }
